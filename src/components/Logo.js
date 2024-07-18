e32d874f6070e2e2370e351bf3303834f6e93503
Shubhangi <shubhangishubhra@gmail.com>
2024-07-18 19:01:17 +0530

    logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logoSvg from "../assets/groww_2.png";
+import logoSvg from "../assets/logo-3.png";
 import Switcher from "./Switcher";
 
 const Logo = () => {
@@ -11,7 +11,7 @@ const Logo = () => {
         className="absolute top-[1.5rem] left-[1.5rem] text-lg text-cyan flex items-center"
         style={{ textDecoration: "none" }}
       >
-        <img src={logoSvg} alt="CryptoBucks" style={{ width: "190px", height: "50px", marginRight: "5px" }} />
+        <img src={logoSvg} alt="CryptoBucks" style={{ width: "130px", height: "100px", marginRight: "5px" }} />
         
       </Link>
       <div className="absolute top-[1.5rem] right-[1.5rem]">
